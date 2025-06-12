c7066b14fd0c2bb752f62539f9ffa9bfb7081c60
chhoflac <chhoflac@student.42.fr>
2025-06-12 17:06:55 +0200

    class ok, function in progress

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,24 +6,30 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 13:46:30 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/27 14:34:48 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/06/12 16:06:30 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <iostream>
+#include <algorithm>
 
 Span::Span(){}
 
-Span::Span(unsigned int n){
+Span::Span(unsigned int n) : max(n){
 	
 }
 
-Span::Span(const Span &src){
-	
+Span::Span(const Span &src) : max(src.max){
+	contain = src.contain;
 }
 
 Span &Span::operator=(const Span &src){
-	
+	if (this != &src){
+		this->max = src.max;
+		this->contain = src.contain;
+	}
+	return (*this);
 }
 
 Span::~Span(){
@@ -31,13 +37,41 @@ Span::~Span(){
 }
 
 void Span::addNumber(int n){
-	
+	if (contain.size() < max){
+		this->contain.insert(n);
+		return;
+	}
+	throw tooManyNumbersException();
 }
 
-int Span::shortestSpan(){
-	
+int Span::shortestSpan()const{
+	if (contain.size() < 2)
+		throw notEnoughNumbersException();
+	int tmp;
+	std::multiset<int>::const_iterator it = contain.begin();
+	std::multiset<int>::const_iterator next = it;
+	++next;
+
+	tmp = *next - *it;
+	while (next != contain.end()){
+		if (tmp > (*next - *it))
+			tmp = *next - *it; 
+		++next;
+		++it;
+	}
+	return (tmp);
 }
 
-int Span::longestSpan(){
-	
+int Span::longestSpan()const{
+	if (contain.size() < 2)
+		throw notEnoughNumbersException();
+	return(*contain.rbegin()- *contain.begin());
+}
+
+const char* Span::tooManyNumbersException::what() const throw(){
+	return ("Error ! too many numbers in multiset container");
+}
+
+const char*Span::notEnoughNumbersException::what() const throw(){
+	return ("Error ! not Enough numbers");
 }
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,12 +6,22 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 13:45:34 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/27 13:46:23 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/06/12 16:12:55 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Span.hpp"
 
-int main(void){
-	
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
 }
